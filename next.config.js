bde56fcb7277b5e2862fe5c3061a12573337f3db
eduardorahal <eduardorahal@gmail.com>
2024-01-11 16:57:47 -0300

    alterado noxet.config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
-    concurrentFeatures: true,
-    swcMinify: true
+    
 }
 
 module.exports = nextConfig
