1dfac68bcc08e01248118b1a1876902418d6da3f
eduardorahal <eduardorahal@gmail.com>
2024-05-17 11:09:53 -0300

    visualizaçao de Menu com marcador de qual item está em uso

--- app/components/Menu/ItensMenu.jsx
+++ app/components/Menu/ItensMenu.jsx
@@ -14,28 +14,43 @@ import PersonIcon from '@mui/icons-material/Person';
 import LogoutIcon from '@mui/icons-material/Logout';
 import Link from 'next/link';
 
-export const mainListItems = (
-  <div>
-    <ListItem button component={Link} href="/">
-      <ListItemIcon>
-        <DashboardIcon />
-      </ListItemIcon>
-      <ListItemText primary="Visão Geral" />
-    </ListItem>
-    <ListItem button component={Link} href="/pix">
-      <ListItemIcon>
-        <LayersIcon />
-      </ListItemIcon>
-      <ListItemText primary="PIX" />
-    </ListItem>
-    <ListItem button component={Link} href="/ccs">
-      <ListItemIcon>
-        <BarChartIcon />
-      </ListItemIcon>
-      <ListItemText primary="CCS" />
-    </ListItem>
-  </div>
-);
+export const mainListItems = () => {
+
+  const [selectedIndex, setSelectedIndex] = React.useState(0);
+
+  const handleListItemClick = (event, index) => {
+    setSelectedIndex(index);
+  }
+
+  return (
+    <div>
+      <ListItem button component={Link} href="/" 
+        selected={selectedIndex === 0}
+        onClick={(event) => handleListItemClick(event, 0)}>
+        <ListItemIcon>
+          <DashboardIcon />
+        </ListItemIcon>
+        <ListItemText primary="Visão Geral" />
+      </ListItem>
+      <ListItem button component={Link} href="/pix"
+        selected={selectedIndex === 1}
+        onClick={(event) => handleListItemClick(event, 1)}>
+        <ListItemIcon>
+          <LayersIcon />
+        </ListItemIcon>
+        <ListItemText primary="PIX" />
+      </ListItem>
+      <ListItem button component={Link} href="/ccs"
+      selected={selectedIndex === 2}
+      onClick={(event) => handleListItemClick(event, 2)}>
+        <ListItemIcon>
+          <BarChartIcon />
+        </ListItemIcon>
+        <ListItemText primary="CCS" />
+      </ListItem>
+    </div >
+  )
+};
 
 export const secondaryListItems = (
   <div>
